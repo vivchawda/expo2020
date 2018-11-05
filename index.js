0f1dfcf5c8007aabe95ec30ee1f4ae7798114964
vivchawda <vivchawda@gmail.com>
2018-11-05 20:44:25 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var animation = lottie.loadAnimation({
-  container: document.getElementById('bm'), // the dom element that will contain the animation
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'data.json' // the path to the animation json
-});
