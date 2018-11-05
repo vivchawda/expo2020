257a0b09144af440c7993c41ce50e2f00ad09cfb
vivchawda <vivchawda@gmail.com>
2018-11-05 20:37:01 +0530

    First Add

---
+++ index.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('bm'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json' // the path to the animation json
+});
